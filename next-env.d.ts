2dc43483a4443b5a916c5de6e48252c54c351b8e
WonderDev21 <wonder.dev21@gmail.com>
2022-08-07 18:55:50 +0900

    fix: updated style config

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,4 +3,7 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-declare module '*.css';
+declare module '*.css' {
+    const content: string;
+    export default content;
+}
